c93b8088b2b7f38d53303a34bcbac3d61a40c1f5
Prince <prince935059@gmail.com>
2025-11-18 15:20:36 +0530

    update code

--- router/listings.js
+++ router/listings.js
@@ -34,8 +34,8 @@ router.get("/:id",  wrapAsync(async(req, res, next) =>{
 //Create Route
 router.post("/", validateListing,wrapAsync(async(req, res, next) => {
     const newListing = new Listing(req.body.listing);
-    
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 })
 );
