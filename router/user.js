350b9f8fb5334ea8c2341f8768cd25ab3d67dafe
Prince <prince935059@gmail.com>
2025-11-29 17:49:52 +0530

    update code

--- router/user.js
+++ router/user.js
@@ -15,8 +15,13 @@ router.post("/signup", wrapAsync(async(req, res) =>{
         const newUser = new User({email, username});
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser);
-        req.flash("success", `Hi ${username}, Welcome to WanderList! Your account has been created successfully.`);
-        res.redirect("/listings");
+        req.login(registeredUser, (err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success", `Hi ${username}, Welcome to WanderList! Your account has been created successfully.`);
+            res.redirect("/listings");
+        });
     }catch(err){
         req.flash("error", err.message);
         res.redirect("/signup");
@@ -42,5 +47,7 @@ router.get("/logout", (req, res) =>{
         res.redirect("/listings");
     });
 });
+
+
 
 module.exports = router;
