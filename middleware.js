0c8ce577ccdc80e7f9c30b6c9c4f9ed84e86d98f
Prince <prince935059@gmail.com>
2025-11-30 13:51:25 +0530

    update code

--- middleware.js
+++ middleware.js
@@ -1,9 +1,17 @@
 module.exports.isLoggedIn = (req, res, next) =>{
-    console.log("middleware are checking: ",req.user);
+    // console.log("middleware are checking: ",req.user);
+    // console.log(req.path, "..", req.originalUrl);
      if(!req.isAuthenticated()){
         req.flash("error", "you must be logged in to create listing!");
-        // return res.redirect("/listings");
+        req.session.redirectUrl = req.originalUrl;
         return res.redirect("/login");
     }
     next();
-}
+};
+
+module.exports.saveRedirectUrl = (req, res, next) =>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
