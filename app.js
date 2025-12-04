9567fa7245f1fe630fff309ab93ce15a155110a5
Prince <prince935059@gmail.com>
2025-12-04 13:18:05 +0530

    update code

--- app.js
+++ app.js
@@ -62,16 +62,6 @@ app.use((req, res, next) =>{
     next();
 });
 
-// app.get("/demouser", async(req, res) =>{
-//     let fakeUser = new User({
-//         email: "student@gmail.com",
-//         username: "delta-student"
-//     });
-
-//     let registeredUser = await User.register(fakeUser, "helloWorld");
-//     res.send(registeredUser);
-// });
-
 //Restructuring Listings
 app.use("/listings", listingRouter);
 
