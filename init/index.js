40ae28bd7ee04cca1272a2dc1b803c5a4cacb800
Prince <prince935059@gmail.com>
2025-12-08 16:51:58 +0530

    update code

--- init/index.js
+++ init/index.js
@@ -10,15 +10,33 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+// const initDB = async () => {
+//     await Listing.deleteMany({});
+//     const newData = initData.data.map(obj => ({
+//         ...obj,
+//         owner: "6935576f0a88abd1fbb9db4d"  // add field here (id/owner)
+//     }));
+//     await Listing.insertMany(newData);
+//     console.log("data was initialized");
+// }
+
 const initDB = async () => {
     await Listing.deleteMany({});
+
+    const owners = [
+        "69355ef54a90cc72e2338aa2",
+        "6935576f0a88abd1fbb9db4d",
+        "693552153dd0a11db4515e94",
+        "69368bd994cce9b27d86d1d0"
+    ];
+
     const newData = initData.data.map(obj => ({
         ...obj,
-        owner: "692c058fbca529921b45512f"  // add field here (id/owner)
+        owner: owners[Math.floor(Math.random() * owners.length)]  // RANDOM owner
     }));
-    await Listing.insertMany(newData);
-    console.log("data was initialized");
-}
 
+    await Listing.insertMany(newData);
+    console.log("data initialized with random owners");
+}
 
 initDB();
