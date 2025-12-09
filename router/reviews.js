39747c69349fb89715e2b054f41ea775b9627235
Prince <prince935059@gmail.com>
2025-12-09 17:54:01 +0530

    update code

--- router/reviews.js
+++ router/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview, isReviewAuthor, isLoggedIn} = require("../middleware.js");
+const {validateReview, isReviewAuthor, isLoggedIn, isOwner} = require("../middleware.js");
 
 //Reviews
 //Post review Route
@@ -31,6 +31,26 @@ router.delete("/:reviewId",isLoggedIn, isReviewAuthor, wrapAsync(async(req, res)
     res.redirect(`/listings/${id}`);
 }));
 
+//Edit Route
+router.get("/:reviewId/edit", isLoggedIn, isReviewAuthor,wrapAsync(async(req, res) =>{
+    let {reviewId, id} = req.params;
+    const listing = await Listing.findById(id);
+    const review = await Review.findById(reviewId);
+    console.log("reivew detail: ",review);
+      if(!review){
+            req.flash("error", "Review you requested for does not exist!");
+           return res.redirect(`/listings/${id}`);
+        }
+    res.render("listings/editReview.ejs", {review, listing});
+}));
 
+//Update Route
+router.put("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync( async(req, res) =>{
+
+    let {id, reviewId} = req.params;
+    await Review.findByIdAndUpdate(reviewId, {...req.body.review});
+    req.flash("success", "Review Updated!");
+    res.redirect(`/listings/${id}`);
+}));
 
 module.exports = router;
