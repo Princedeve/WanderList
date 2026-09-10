1dab84598aac2f1b39c9397cdcaab905f3fbe01f
Prince <prince935059@gmail.com>
2026-09-10 13:03:43 +0530

    update code

--- public/js/map.js
+++ public/js/map.js
@@ -6,19 +6,35 @@ if (mapElement) {
     const longitude = parseFloat(mapElement.getAttribute("data-lng"));
 
     const title = mapElement.getAttribute("data-title");
-    const location = mapElement.getAttribute("data-location");
 
     console.log("Latitude:", latitude);
     console.log("Longitude:", longitude);
 
-    const map = L.map("map").setView([latitude, longitude], 13);
+    const map = L.map("map").setView([latitude, longitude], 15);
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "&copy; OpenStreetMap contributors"
     }).addTo(map);
 
-    L.marker([latitude, longitude])
-        .addTo(map)
-        .bindPopup(`<b>${title}</b><br>${location}`)
-        .openPopup();
+    const houseIcon = L.divIcon({
+    html: "🏠",
+    className: "house-marker",
+    iconSize: [40, 40],
+    iconAnchor: [20, 40]
+});
+
+const marker = L.marker([latitude, longitude], {
+    icon: houseIcon
+}).addTo(map);
+
+marker.bindTooltip(`
+    <div class="map-tooltip">
+        <h4>${title}</h4>
+        <p>Exact location provided after booking</p>
+    </div>
+`, {
+    direction: "top",
+    offset: [0, -35],
+    opacity: 1
+});
 }
