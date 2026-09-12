c9fa251bc17f750ae26d4159a52a16b695e2bdf3
Prince <prince935059@gmail.com>
2026-09-12 17:08:27 +0530

    update code

--- schema.js
+++ schema.js
@@ -7,6 +7,19 @@ module.exports.listingSchema = Joi.object({
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
+        category: Joi.string()
+            .valid(
+                "Trending",
+                "Room",
+                "Iconic Cities",
+                "Mountains",
+                "Castles",
+                "Amazing Pools",
+                "Camping",
+                "Farms",
+                "Arctic"
+            )
+            .required(),
         image: Joi.string().allow("", null),
     }).required(),
 });
